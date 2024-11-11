9fefbbdfc0d64cd78727008db0354424adc69de7
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-11-11 14:23:07 +0600

    components issue solved

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../assets/components/shared/Navbar";
-import Footer from "../assets/components/shared/Footer";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+
 
 
 const Main = () => {
